d3614e1789cc687154ddc87f97b3f42b192fd135
chokbunthit <chokbunthit@hotmail.com>
2022-01-18 14:12:02 +0700

    sample commit

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+
+
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","./src/views");
+app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-    res.send('Hello Meeting')
+    res.render("index",{username:'Petercrop9999',customers:[{cust_num:"C0001",cust_name:"PIAB"},{cust_num:"C0002",cust_name:"TOYOTA"}]});
 })
 
-app.listen(port,()=>{
-    debug("Listening on port"+ chalk.green(port));
+app.listen(PORT,()=>{
+    debug("Listening on port :"+ chalk.green(PORT));
 })
